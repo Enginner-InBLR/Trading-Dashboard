92e1607107a3e9b80b68d60535ec0907275be3dc
stranger1989 <highway_300_axe1989@yahoo.co.jp>
2023-02-13 11:19:38 +0900

    Feat: add tailwind settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import styles from './page.module.css';
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>Hello World</p>
-      </div>
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </main>
   );
 }
