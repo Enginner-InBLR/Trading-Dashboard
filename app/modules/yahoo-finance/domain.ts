6df769b9171150663fbf48a79e600cbd6d456f47
stranger1989 <highway_300_axe1989@yahoo.co.jp>
2023-02-18 13:25:37 +0900

    Feat: add HistoricalData domain logic

---
+++ app/modules/yahoo-finance/domain.ts
@@ -0,0 +1,31 @@
+import {
+  HistoricalDataResponse,
+  MetaResponse,
+  QuoteResponse,
+} from '@/app/modules/yahoo-finance/type';
+
+export class HistoricalData {
+  public meta: MetaResponse;
+
+  public timestamps: number[];
+
+  public quotes: QuoteResponse;
+
+  constructor(historicalDataResponse: HistoricalDataResponse) {
+    this.meta = historicalDataResponse.chart.result[0].meta;
+    this.timestamps = historicalDataResponse.chart.result[0].timestamp;
+    [this.quotes] = historicalDataResponse.chart.result[0].indicators.quote;
+  }
+
+  public getRegularMarketChange(): number {
+    return this.meta.regularMarketPrice - this.meta.previousClose;
+  }
+
+  public getRegularMarketChangePercentage() {
+    return (this.getRegularMarketChange() / this.meta.regularMarketPrice) * 100;
+  }
+
+  public isRegularMarketChangeIncreased(): boolean {
+    return this.getRegularMarketChange() >= 0;
+  }
+}
