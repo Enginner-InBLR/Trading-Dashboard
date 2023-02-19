6623f55abbce59add38468e942ab86defaa5df78
stranger1989 <highway_300_axe1989@yahoo.co.jp>
2023-02-19 19:35:49 +0900

    Feat: add title in markets page

--- app/markets/page.tsx
+++ app/markets/page.tsx
@@ -5,6 +5,9 @@ const DISPLAY_SYMBOL_LIST = ['^GSPC', '^DJI', '^IXIC'];
 export default function Markets() {
   return (
     <>
+      <h2 className="text-lg font-bold text-gray-900 sm:text-2xl pb-3">
+        Markets
+      </h2>
       {DISPLAY_SYMBOL_LIST.map((symbol) => (
         <>
           {/* @ts-expect-error: https://github.com/vercel/next.js/issues/42292 */}
